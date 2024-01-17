1f4aad0bed86943e954d44bb7b826cd975bc2f9e
Matt <maweiche@gmail.com>
2024-01-17 19:17:42 +0100

    added in nft parser for wallet to select an nft to play with, fixed styling issue with background color not occupying whole screen

--- components/Bird.js
+++ components/Bird.js
@@ -2,15 +2,17 @@
 import React from "react";
 
 const Bird = ({ birdPosition }) => {
-
+	// get the image from the local storage "selectedNft"
+	const imageUrl = localStorage.getItem("selectedNft");
 	return (
 		<img
-			src={"https://media.geeksforgeeks.org/wp-content/uploads/20231211115925/flappy_bird_by_jubaaj_d93bpnj.gif"}
+			src={imageUrl ? imageUrl : "https://media.geeksforgeeks.org/wp-content/uploads/20231211115925/flappy_bird_by_jubaaj_d93bpnj.gif"}
 			alt="bird"
 			className="bird"
 			style={{
 				left: birdPosition.x,
 				top: birdPosition.y,
+				transform: birdPosition.rotation,
 			}}
 			draggable={true}
 		/>
